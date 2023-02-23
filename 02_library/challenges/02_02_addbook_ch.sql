9e0563be2d3d70d8c18fa711231dd44c425af9d6
KGGoodchild <126115253+KGGoodchild@users.noreply.github.com>
2023-02-23 20:59:00 +0000

    my comments

--- 02_library/challenges/02_02_addbook_ch.sql
+++ 02_library/challenges/02_02_addbook_ch.sql
@@ -9,3 +9,15 @@
 -- Author: Jonathan Swift
 -- Published: 1729
 -- ID Number: 4899254401
+
+INSERT INTO Books (Title, Author, Published, Barcode)
+VALUES ('Dracula', 'Bram Stoker', 1987, 4819277482),
+('Gulliver’s Travels into Several Remote Nations of the World',
+'Jonathan Swift', 1729, 4899254401);
+
+SELECT * FROM Books
+WHERE Barcode = 4819277482 OR Barcode = 4899254401;
+--OR
+SELECT * FROM Books
+ORDER BY BookID DESC
+LIMIT 5;
