6d2acbb148f98c32cbaf2dc01ea95ef8ce2e3f85
KGGoodchild <126115253+KGGoodchild@users.noreply.github.com>
2023-02-23 22:52:11 +0000

    notes

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,12 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT * FROM Loans
+
+SELECT COUNT(l.LoanID) AS CheckOuts,
+p.FirstName, p.LastName, p.Email
+FROM Loans l
+JOIN Patrons p ON l.PatronID = p.PatronID
+GROUP BY p.PatronID
+ORDER BY CheckOuts
+LIMIT 15;
--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -3,5 +3,17 @@
 -- Report 1: Show how many titles were published 
 -- in each year.
 
+SELECT Published, COUNT(DISTINCT(Title)) AS PubCount
+FROM Books
+GROUP BY Published
+ORDER BY PubCount DESC;
+
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT COUNT(Loans.LoanID) AS CheckedOut, Books.Title 
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+GROUP BY Books.Title
+ORDER BY CheckedOut DESC
+LIMIT 5;
